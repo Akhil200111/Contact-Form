d8ec9b23ed5e72afd3d93e9e409451cae82646cf
AK <aakhilp1@gmail.com>
2025-08-27 12:29:00 +0530

    create a contact details form

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const { createElement } = require("react");
+
+const name = document.querySelector('.name');
+const phNo = document.querySelector('.phone_no');
+const saveBtn = document.querySelector('.save');
+const userDetails = document.querySelector('.user_details');
+
+saveBtn.addEventListener('click',() =>{
+   
+})
