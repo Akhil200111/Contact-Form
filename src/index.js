9ecc520ada195a4b3917b9b5c6904bb9446e594f
AK <aakhilp1@gmail.com>
2025-08-28 21:22:43 +0530

    add dynamic functionalities

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
-const { createElement } = require("react");
 
-const name = document.querySelector('.name');
+
+const name1 = document.querySelector('.name');
 const phNo = document.querySelector('.phone_no');
-const saveBtn = document.querySelector('.save');
-const userDetails = document.querySelector('.user_details');
+const saveBtn = document.getElementById('save');
 
-saveBtn.addEventListener('click',() =>{
-   
+
+const userDetails = document.querySelector(".user_details");
+
+
+
+
+saveBtn.addEventListener('click',(e) =>{
+    e.preventDefault();
+    const name2 = name1.value.trim(); 
+    const phno1 = phNo.value.trim();
+   if(name2 && phno1){
+  const li =  document.createElement("li");
+  li.textContent = name2 + " " + phno1;
+
+  console.log(li);
+  userDetails.appendChild(li);
+   }
 })
+
+
